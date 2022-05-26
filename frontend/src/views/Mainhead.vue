768fe47d9c0c5e3a59de165e934023908fefb5c9
Mateusz Wicki <mateuszwicki1@gmail.com>
2022-05-26 22:20:22 +0200

    navbar

--- frontend/src/views/Mainhead.vue
+++ frontend/src/views/Mainhead.vue
@@ -2,10 +2,14 @@
   <v-app-bar
     app
     flat
-    style="box-shadow: 0 1rem 3rem rgba(0, 0, 0, 0.175) !important; height: 75px; z-index: 9999;"
+    style="
+      box-shadow: 0 1rem 3rem rgba(0, 0, 0, 0.175) !important;
+      height: 75px;
+      z-index: 9999;
+    "
     class="white px-16"
   >
-    <v-row class="d-flex align-center mx-auto" style="max-width: 1160px;">
+    <v-row class="d-flex align-center mx-auto" style="max-width: 1160px">
       <v-col class="pt-5">
         <v-img
           class="logo"
@@ -17,12 +21,108 @@
         ></v-img>
       </v-col>
       <v-spacer></v-spacer>
-      <v-btn v-if="!isAuthenticated" text :to="'/auth/login'" color="customPrimary" class="text-subtitle-1 font-weight-medium"> Zaloguj </v-btn>
-      <v-btn v-if="!isAuthenticated" text :to="'/auth/register'" color="customPrimary" class="text-subtitle-1 font-weight-medium">
+      <v-menu offset-y >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="primary" class="d-md-none d-inline-block" text v-bind="attrs" v-on="on">
+            <v-icon>mdi-menu</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-if="!isAuthenticated">
+            <v-btn
+              text
+              :to="'/auth/login'"
+              color="customPrimary"
+              class="text-subtitle-1 font-weight-medium"
+            >
+              Zaloguj
+            </v-btn>
+          </v-list-item>
+          <v-list-item v-if="!isAuthenticated">
+            <v-btn
+              text
+              :to="'/auth/register'"
+              color="customPrimary"
+              class="text-caption font-weight-medium"
+            >
+              Rejestracja
+            </v-btn>
+          </v-list-item>
+          <v-list-item v-if="isAuthenticated">
+            <v-btn
+              text
+              :to="`/user/${user}`"
+              color="customPrimary"
+              class="text-caption font-weight-medium"
+            >
+              {{ user }}
+            </v-btn>
+          </v-list-item>
+          <v-list-item v-if="isAuthenticated">
+            <v-btn
+              v-if="isAuthenticated"
+              text
+              :to="'/posts/addplace'"
+              color="customPrimary"
+              class="text-caption font-weight-medium"
+              >Dodaj miejsce</v-btn
+            >
+          </v-list-item>
+          <v-list-item v-if="isAuthenticated">
+            <v-btn
+              v-if="isAuthenticated"
+              text
+              @click="logout"
+              color="customPrimary"
+              class="text-caption font-weight-medium"
+              >Wyloguj</v-btn
+            >
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <v-btn
+        v-if="!isAuthenticated"
+        text
+        :to="'/auth/login'"
+        color="customPrimary"
+        class="text-subtitle-1 font-weight-medium d-none d-md-inline-flex"
+      >
+        Zaloguj
+      </v-btn>
+      <v-btn
+        v-if="!isAuthenticated"
+        text
+        :to="'/auth/register'"
+        color="customPrimary"
+        class="text-caption font-weight-medium d-none d-md-inline-flex"
+      >
         Rejestracja
       </v-btn>
-      <v-btn v-if="isAuthenticated" text :to="`/user/${user}`" color="customPrimary" class="text-subtitle-1 font-weight-medium"> {{user}} </v-btn>
-      <v-btn v-if="isAuthenticated" text @click="logout" color="customPrimary" class="text-subtitle-1 font-weight-medium">Wyloguj</v-btn>
+      <v-btn
+        v-if="isAuthenticated"
+        text
+        :to="`/user/${user}`"
+        color="customPrimary"
+        class="text-caption font-weight-medium d-none d-md-inline-flex"
+      >
+        {{ user }}
+      </v-btn>
+      <v-btn
+        v-if="isAuthenticated"
+        text
+        :to="'/posts/addplace'"
+        color="customPrimary"
+        class="text-caption font-weight-medium d-none d-md-inline-flex"
+        >Dodaj miejsce</v-btn
+      >
+      <v-btn
+        v-if="isAuthenticated"
+        text
+        @click="logout"
+        color="customPrimary"
+        class="text-caption font-weight-medium d-none d-md-inline-block"
+        >Wyloguj</v-btn
+      >
     </v-row>
   </v-app-bar>
 </template>
@@ -31,7 +131,14 @@ import { mapState, mapActions } from "vuex";
 import { axiosAPI } from "../axiosAPI";
 export default {
   data() {
-    return {};
+    return {
+      items: [
+        { title: "Click Me" },
+        { title: "Click Me" },
+        { title: "Click Me" },
+        { title: "Click Me 2" },
+      ],
+    };
   },
   computed: {
     ...mapState({
@@ -52,7 +159,7 @@ export default {
         .catch();
     },
     home() {
-      this.$router.push("/")
+      this.$router.push("/");
     },
   },
 };
